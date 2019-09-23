3aa80a67af2b453933f3a5783b37a9fef9c13cbf
Oguz Sabitay <oguzsabitay@gmail.com>
2019-09-23 22:52:34 +0300

    Stared to do the actual project

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
+import java.util.Queue;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("This is somewhat cool. I guess.");
+        // Ariel 1, Betty 2, Carol 3, Daenerys 4, Emma 5
+        // Vivaldi 1, Xavi 2, Yuri 3, Wolverine 4, Zerq 5
+
+
     }
 }
